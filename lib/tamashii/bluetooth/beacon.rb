e7b53773f74b539239a669e4bbce2369a8b1a6b3
蒼時弦也 <elct9620@frost.tw>
2018-04-14 15:58:05 +0800

    Provide Line Beacon support

---
+++ lib/tamashii/bluetooth/beacon.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Tamashii
+  module Bluetooth
+    # Beacon Support
+    module Beacon
+      autoload :Line, 'tamashii/bluetooth/beacon/line'
+    end
+  end
+end
---
+++ lib/tamashii/bluetooth/beacon/line.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+module Tamashii
+  module Bluetooth
+    module Beacon
+      # Line Beacon Format
+      class Line < Advertisment
+        ADTYPE_FLAGS = 0x01
+        ADTYPE_COMPLETE_16_BIT_SERVICE_UUID = 0x03
+        ADTYPE_SERVICE_DATA = 0x16
+        UUID16LE_FOR_LINECORP = [0x6F, 0xFE].freeze
+
+        attr_reader :hardware_id
+
+        def initialize(id)
+          # TODO: Support append message
+          @hardware_id = id
+          @data = create_data
+        end
+
+        def frame_type
+          [0x02]
+        end
+
+        def tx_power
+          [0x7F]
+        end
+
+        def hardware_hex
+          hardware_id.scan(/../).map(&:hex)
+        end
+
+        def beacon_frame
+          [
+            frame_type,
+            hardware_hex,
+            tx_power,
+            [0x00] # Message
+          ].flatten
+        end
+
+        def create_data
+          [
+            flag,
+            completed_uuid,
+            service_data
+          ].flatten
+        end
+
+        def flag
+          add_structure(ADTYPE_FLAGS, [0x06])
+        end
+
+        def completed_uuid
+          add_structure(
+            ADTYPE_COMPLETE_16_BIT_SERVICE_UUID,
+            UUID16LE_FOR_LINECORP
+          )
+        end
+
+        def service_data
+          add_structure(
+            ADTYPE_SERVICE_DATA,
+            UUID16LE_FOR_LINECORP + beacon_frame
+          )
+        end
+
+        def add_structure(type, buffer)
+          header = [buffer.length + 1, type]
+          [header, buffer]
+        end
+      end
+    end
+  end
+end
